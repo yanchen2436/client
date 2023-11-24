463a66f70b06866911f403df16ae88dc2ce8d0db
yanchen <xiaoqiyanchen2436@163.com>
2023-11-24 17:28:40 +0800

    流程测试2

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo mount -t nfs 192.168.10.200:/home/admin/share/image/img /mnt/img
+sudo mount -t nfs 192.168.10.200:/home/admin/share /mnt/img
 cd /home/nano/client
 python3 close.py
 python3 client_log.py
