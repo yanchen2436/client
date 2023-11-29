83f985be590b1dc7a24fe2e061beaf185aefc6fa
yanchen <xiaoqiyanchen2436@163.com>
2023-11-29 10:38:53 +0800

    流程测试11.29
    自动挂载优化
    端面拍照优化

--- cube_save.py
+++ cube_save.py
@@ -4,14 +4,19 @@ import os
 import shutil
 import subprocess
 
-def capture_and_run_scripts(camera_indices, output_folder, open_script_path, close_script_path):
-    # Clear the existing content of the output folder
-    if os.path.exists(output_folder):
-        shutil.rmtree(output_folder)
-    os.makedirs(output_folder)
 
-    # Set the permissions of the output folder to 777
-    os.chmod(output_folder, 0o777)
+def clear_folder(folder_path):
+    # Clear the existing content of the folder
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+    os.makedirs(folder_path, exist_ok=True)
+    os.chmod(folder_path, 0o777)
+
+
+def capture_and_run_scripts(camera_indices, output_folders, open_script_path, close_script_path):
+    for output_folder in output_folders:
+        # Clear the existing content of the output folder
+        clear_folder(output_folder)
 
     # Open the cameras
     caps = [cv2.VideoCapture(index) for index in camera_indices]
@@ -28,9 +33,7 @@ def capture_and_run_scripts(camera_indices, output_folder, open_script_path, clo
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     # Capture a single frame from each camera with a delay of 100ms
-    frames = [None] * len(caps)
-    for i, cap in enumerate(caps):
-        frames[i] = cap.read()[1]
+    frames = [cap.read()[1] for cap in caps]
 
     # Check if the images are read correctly
     for i, frame in enumerate(frames):
@@ -40,22 +43,30 @@ def capture_and_run_scripts(camera_indices, output_folder, open_script_path, clo
 
     # Save the images to the output folder with customized filenames
     for i, frame in enumerate(frames):
-        initial_image_path = os.path.join(output_folder, f"cam_c_{camera_indices[i]}.jpg")
-        cv2.imwrite(initial_image_path, frame)
-        os.chmod(initial_image_path, 0o777)
+        image_path = os.path.join(output_folders[i], f"cam_{i}_C.jpg")
+        cv2.imwrite(image_path, frame)
+        os.chmod(image_path, 0o777)
 
     # Release the cameras
     for cap in caps:
         cap.release()
 
     # Display a message
-    print("Initial images captured successfully.")
+    print(f"Initial images captured successfully for cameras {camera_indices} in folders {output_folders}.")
 
-    # Run the open.py script
-    subprocess.run(["python3","/home/nano/client/open.py"])
+    # Run open.py script
+    subprocess.run(["python3", open_script_path])
 
     # Wait for a moment (you can adjust the sleep duration as needed)
     time.sleep(0.1)
+
+    # Open the cameras again
+    caps = [cv2.VideoCapture(index) for index in camera_indices]
+
+    # Set the image size for all cameras to 1280x1080
+    for cap in caps:
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     # Capture another frame from each camera after running open.py
     frames_after_open = [cap.read()[1] for cap in caps]
@@ -68,25 +79,28 @@ def capture_and_run_scripts(camera_indices, output_folder, open_script_path, clo
 
     # Save the images after open.py to the output folder with customized filenames
     for i, frame_after_open in enumerate(frames_after_open):
-        after_open_image_path = os.path.join(output_folder, f"cam_o_{camera_indices[i]}.jpg")
+        after_open_image_path = os.path.join(output_folders[i], f"cam_{i}_O.jpg")
         cv2.imwrite(after_open_image_path, frame_after_open)
         os.chmod(after_open_image_path, 0o777)
 
-    # Run the close.py script
-    subprocess.run(["python3","/home/nano/client/close.py"])
+    # Release the cameras after running open.py
+    for cap in caps:
+        cap.release()
+
+    # Run close.py script
+    subprocess.run(["python3", close_script_path])
 
     # Display a message
-    print("Scripts executed successfully.")
+    print(f"Scripts executed successfully for cameras {camera_indices} in folders {output_folders}.")
+
 
 # Set the camera indices and output folders
 camera_indices = [0, 1]  # You can modify this list based on your actual camera indices
 output_folder_cam0 = os.path.expanduser("/mnt/img/cube_img/f_cam0")  # Output folder path for camera 0
 output_folder_cam1 = os.path.expanduser("/mnt/img/cube_img/f_cam1")  # Output folder path for camera 1
+output_folders = [output_folder_cam0, output_folder_cam1]
 open_script_path = "/home/nano/client/open.py"
 close_script_path = "/home/nano/client/close.py"
 
-# Call the capture_and_run_scripts function for camera 0
-capture_and_run_scripts([camera_indices[0]], output_folder_cam0, open_script_path, close_script_path)
-
-# Call the capture_and_run_scripts function for camera 1
-capture_and_run_scripts([camera_indices[1]], output_folder_cam1, open_script_path, close_script_path)
+# Call the capture_and_run_scripts function
+capture_and_run_scripts(camera_indices, output_folders, open_script_path, close_script_path)
--- init.sh
+++ init.sh
@@ -1,8 +1,37 @@
 #! /bin/bash
 
-sudo mount -t nfs 192.168.10.200:/home/admin/share /mnt/img
+# 设置远程服务器的IP地址，可以替换成你自己的服务器
+remote_server="192.168.10.200"
 
-cd /home/nano/client
-python3 close.py
-python3 client_log.py
+# 设置最大尝试次数
+max_attempts=10
+current_attempt=1
 
+# 循环直到 ping 成功或达到最大尝试次数
+while [ $current_attempt -le $max_attempts ]; do
+  # 尝试ping远程服务器一次
+  ping -c 1 $remote_server > /dev/null 2>&1
+
+  # 检查ping命令的退出状态
+  if [ $? -eq 0 ]; then
+    echo "网络已启动，可以连接到远程服务器。"
+
+    # 在这里可以继续执行你的其他操作
+    sudo mount -t nfs 192.168.10.200:/home/admin/share /mnt/img
+
+    cd /home/nano/client
+    python3 close.py
+    python3 client_log.py
+
+    break  # 跳出循环，因为成功了
+
+  else
+    echo "第 $current_attempt 次尝试失败，等待3秒后重试..."
+    sleep 3
+    ((current_attempt++))
+  fi
+done
+
+if [ $current_attempt -gt $max_attempts ]; then
+  echo "尝试达到最大次数，无法连接到远程服务器。"
+fi
